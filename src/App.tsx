7e51cce3653fd38b5f47b6a7e68ce5e0bc1aa015
SarikViper <dzamolovsardor5@gmail.com>
2025-02-28 21:08:27 +0500

    data accepted

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { Routes } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-gray-100 text-center">
+        <h1 className="text-yellow-700">Hello TS</h1> 
+      </div>
+    </>
+  )
+}
+
+export default App
