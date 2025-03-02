0d39396661a658e70cda0f6a67f90ac879a9b45f
SarikViper <dzamolovsardor5@gmail.com>
2025-03-02 16:51:01 +0500

    data accepted

--- src/containers/main-catalogue.tsx
+++ src/containers/main-catalogue.tsx
@@ -9,14 +9,14 @@ function MainCatalogue() {
             <div className="relative flex flex-col gap-2 items-center my-5 h-full text-center text-white">
                 <h1 className="text-8xl font-bold">Каталог нашей продукции</h1>
                 <div className="flex my-5 gap-10 items-center justify-center ">
-                    <p>Все</p>
-                    <p>Молоко</p>
-                    <p>Сырки</p>
-                    <p>Сыры</p>
-                    <p>Сметана,Каймак,Сливки</p>
-                    <p>Йогурты</p>
-                    <p>Кисломолочная продукция</p>
-                    <p>Масло</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Все</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Молоко</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Сырки</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Сыры</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Сметана, Каймак, Сливки</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Йогурты</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Кисломолочная продукция</p>
+                    <p className="cursor-pointer hover:text-yellow-300 hover:underline text-xl">Масло</p>
                 </div>
             </div>
             <div className="grid grid-cols-4 gap-5 p-5">
