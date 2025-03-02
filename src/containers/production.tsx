53aa80e8767569c6f21e397fa017b0bad08304b4
SarikViper <dzamolovsardor5@gmail.com>
2025-03-02 18:12:10 +0500

    some changes

--- src/containers/production.tsx
+++ src/containers/production.tsx
@@ -5,7 +5,7 @@ const Products = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="bg-white p-8 max-w-6xl mx-auto my-20">
+        <div className="bg-white p-8 max-w-6xl mx-auto mt-20">
             <div className="flex justify-between items-center mb-6">
                 <h1 className="text-5xl font-bold text-green-700">
                     Продукция <br /> <span className="text-green-500">Puremilky</span>
@@ -30,29 +30,30 @@ const Products = () => {
                         требованиям европейского качества</span>
                 </div>
             </div>
-            <div className="my-15 flex flex-col justify-between items-center">
-                <div className="flex justify-center items-start">
-                <div>
+            <div className="mt-50 flex gap-25 ">
+                <div className="flex gap-30 ">
+                <div className="flex flex-col items-center">
                     <h2 className="text-green-700 text-5xl font-bold">10m</h2>
-                    <p className="text-gray-700 text-lg">Продуктов произведено</p>
+                    <p className="text-gray-700 text-xl">Продуктов произведено</p>
                 </div>
-                <div>
-                    <h2 className="text-green-700 text-5xl font-bold">10 000</h2>
-                    <p className="text-gray-700 text-lg">
-                        Магазинов Узбекистана <br /> продают товары от Puremilky
-                    </p>
-                </div>
-                </div>
-                <div className="flex justify-center items-end">
-                <div>
+                    <div className="flex flex-col my-50">
                     <h2 className="text-green-700 text-5xl font-bold">23</h2>
-                    <p className="text-gray-700 text-lg">
+                    <p className="text-gray-700 text-xl">
                         Крупных городов СНГ <br /> пользуются продукцией Puremilky
                     </p>
                 </div>
-                    <div>
+                </div>
+                
+                <div className="flex gap-30">
+                <div className="flex flex-col ">
+                    <h2 className="text-green-700 text-5xl font-bold">10 000</h2>
+                    <p className="text-gray-700 text-xl">
+                        Магазинов Узбекистана <br /> продают товары от Puremilky
+                    </p>
+                </div>
+                    <div className="flex flex-col my-50 ">
                         <h2 className="text-green-700 text-5xl font-bold">10m</h2>
-                        <p className="text-gray-700 text-lg">Постоянных покупателей продукции Puremilky</p>
+                        <p className="text-gray-700 text-xl">Постоянных покупателей продукции Puremilky</p>
                     </div>
                 </div>
             </div>
--- src/containers/profits.tsx
+++ src/containers/profits.tsx
@@ -1,26 +1,30 @@
 const Profits = () => {
     return (
         <>
-            <div className="bg-white p-8 max-w-6xl mx-auto my-20">
-                <div className="flex flex-col items-center mt-10">
+            <div className="bg-white p-8 max-w-6xl mx-auto">
+                <div className="flex flex-col items-center mb-10">
                     <div className="w-full flex justify-between items-center">
                         <h1 className="text-green-700 text-5xl font-bold">Преимущества <br /> продуктов</h1>
                         <span>Puremilky - молочная продукция фермерского производства, <br /> изготавливается согласно <br />
                             требованиям европейского качества</span>
                     </div>
-                    <div className="w-full h-300 bg-[url('/cow.png')] bg-no-repeat bg-cover bg-center my-25">
-                        <div>
-                            <div className="items-start bg-green-600 text-white w-100 h-35 p-5 rounded-lg">
+                    <div className="w-full h-300 bg-[url('/cow.png')] bg-no-repeat bg-cover bg-center my-25 justify-center items-center flex">
+                        <div className="flex flex-col gap-40">
+                            <div className="flex gap-50">
+                            <div className="bg-green-600 text-white w-100 h-35 p-5 rounded-lg">
                                 <p className="text-left">Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского качества</p>
                             </div>
-                            <div className="items-start bg-green-600 text-white w-100 h-35 p-5 rounded-lg">
+                            <div className="bg-green-600 text-white my-25 w-100 h-35 p-5 rounded-lg">
                                 <p className="text-left">Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского качества</p>
                             </div>
-                            <div className="items-start bg-green-600 text-white w-100 h-35 p-5 rounded-lg">
+                            </div>
+                            <div className="flex gap-50">
+                            <div className="bg-green-600 text-white w-100 h-35 p-5 rounded-lg">
                                 <p className="text-left">Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского качества</p>
                             </div>
-                            <div className="items-start bg-green-600 text-white w-100 h-35 p-5 rounded-lg">
+                            <div className="bg-green-600 my-25 text-white w-100 h-35 p-5 rounded-lg">
                                 <p className="text-left">Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского качества</p>
+                            </div>
                             </div>
                         </div>
                     </div>
