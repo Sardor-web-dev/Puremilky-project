f243f719183c74c18c903a8020cfa69328ac3f50
SarikViper <dzamolovsardor5@gmail.com>
2025-03-02 23:33:00 +0500

    data accepted

--- src/containers/profits.tsx
+++ src/containers/profits.tsx
@@ -8,21 +8,21 @@ const Profits = () => {
             </p>
             <div className="relative">
                 <img src="/cow.png" alt="Коровы на ферме" className="w-full rounded-lg" />
-                
+
                 <div className="absolute top-5 left-5 bg-green-600 text-white p-3 rounded-lg w-56 shadow-lg">
-                    <p>Puremilky - молочная фермерского производства, изготавливается согласно требованиям европейского</p>
+                    <p>Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского</p>
                 </div>
-                
-                <div className="absolute top-1/3 right-5 bg-green-600 text-white p-3 rounded-lg w-56 shadow-lg">
-                    <p>Puremilky - молочная фермерского производства, изготавливается согласно требованиям европейского</p>
+
+                <div className="hidden sm:block absolute top-1/3 right-5 bg-green-600 text-white p-3 rounded-lg w-56 shadow-lg">
+                    <p>Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского</p>
                 </div>
-                
-                <div className="absolute bottom-1/3 left-5 bg-green-600 text-white p-3 rounded-lg w-56 shadow-lg">
-                    <p>Puremilky - молочная фермерского производства, изготавливается согласно требованиям европейского</p>
+
+                <div className="hidden sm:block absolute bottom-1/3 left-5 bg-green-600 text-white p-3 rounded-lg w-56 shadow-lg">
+                    <p>Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского</p>
                 </div>
-                
+
                 <div className="absolute bottom-5 right-5 bg-green-600 text-white p-3 rounded-lg w-56 shadow-lg">
-                    <p>Puremilky - молочная фермерского производства, изготавливается согласно требованиям европейского</p>
+                    <p>Puremilky - молочная продукция фермерского производства, изготавливается согласно требованиям европейского</p>
                 </div>
             </div>
         </div>
