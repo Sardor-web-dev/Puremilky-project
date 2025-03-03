bcfd73cc3d52dce9254d3755206431e5cd067b6e
SarikViper <dzamolovsardor5@gmail.com>
2025-03-03 17:03:17 +0500

    some changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,58 +1,95 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
-        <>
-            <div className="flex gap-10 lg:flex-row lg:gap-70 mx-auto max-w-5xl p-6 lg:p-10 rounded-xl items-center shadow-md">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-3xl sm:text-5xl text-green-700 font-bold">
-                        ОСТАЛИСЬ <br className="hidden sm:block" /> ВОПРОСЫ?
-                    </h1>
-                    <p className="text-lg sm:text-xl text-green-600 font-medium mt-2">
-                        Заполните небольшую заявку <br className="hidden sm:block" />
-                        и мы свяжемся с вами <br className="hidden sm:block" />
-                        в течение 2 рабочих дней
-                    </p>
-                </div>
+      <footer className="flex items-center justify-center bg-[#00A23A] text-white py-[53px] relative mt-[60px] lg:mt-[213px]">
+        <div className="lg:bg-[url('/kefir.png')] bg-no-repeat bg-cover bg-center absolute top-0 w-full h-[190px]"></div>
+        
+        <div className="relative z-10 w-full">
+          <div className="max-w-screen-xl mx-auto flex justify-center">
+            <div className="bg-white text-[#00A23A] rounded-xl p-[21px] lg:p-[60px] flex items-center gap-[40px] mt-[-100px] lg:gap-[250px]"> 
+              <div>
+                <h3 className="text-[18px] lg:text-[38px] font-bold">ОСТАЛИСЬ <br /> ВОПРОСЫ?</h3>
+                <p className="mt-[14px] text-[12px] lg:text-[22px]">Заполните небольшую заявку <br /> и мы свяжемся с вами <br /> в течение 2х рабочих дней</p>
+              </div>
+              <div className="flex flex-col gap-[10px] lg:gap-[14px]">
+                <input type="text" placeholder="Ваше имя" className="border-none text-[#717171] rounded-lg py-[10px] px-[17px] lg:p-[18px] bg-[#ECECEC]" />
+                <input type="text" placeholder="Номер телефона" className="border-none text-[#717171] rounded-lg py-[10px] px-[17px] lg:p-[18px] bg-[#ECECEC]" />
+                <button className="bg-[#00A23A] text-white py-[10px] px-[31px] lg:py-[18px] lg:px-[80px] rounded-lg cursor-pointer">Отправить заявку</button>
+              </div>
+            </div>
+          </div>
 
-                <form className="flex flex-col gap-4 w-full sm:w-auto items-center lg:items-start" name="request">
-                    <input className="w-full sm:w-80 h-12 p-4 bg-gray-200 rounded-xl" type="text" placeholder="Ваше имя" />
-                    <input className="w-full sm:w-80 h-12 p-4 bg-gray-200 rounded-xl" type="tel" placeholder="Номер телефона" />
-                    <button className="w-full sm:w-80 h-12 bg-green-600 rounded-xl text-white cursor-pointer">
-                        Отправить заявку
-                    </button>
-                </form>
+          <div className="w-full">
+            <div className="flex items-center justify-center gap-[39px] mt-[30px] lg:gap-[150px] lg:mt-[50px] w-full">
+              <div className="">
+                <img src="/logo.png" alt="" className="hidden lg:block"/>
+              </div>
+              <div className="">
+                <h4 className="text-[18px] lg:text-[40px] font-bold">Меню</h4>
+                <div className="flex items-center justify-center gap-[15px] lg:gap-[132px] mt-[19px]">
+                  <div className="flex flex-col text-[12px] lg:text-[20px] font-normal">
+                    <Link to={"/catalogue"} className="hover:underline">Продукция</Link>
+                    <a href="#" className="hover:underline">Рецепты</a>
+                    <a href="#" className="hover:underline">Блог</a>
+                  </div>
+                  <div className="flex flex-col text-[12px] lg:text-[20px] font-normal">
+                    <a href="#" className="hover:underline">О компании</a>
+                    <a href="#" className="hover:underline">Вакансии</a>
+                    <a href="#" className="hover:underline">Контакты</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="hidden lg:block">
+                <h4 className="text-[18px] lg:text-[40px] font-bold">Контактная информация</h4>
+                <div className="flex items-center gap-[95px] mt-[19px]">
+                  <div className="flex flex-col">
+                    <p className="text-[12px] lg:text-[15px]">Поддержка:</p>
+                    <span className="text-[12px] lg:text-[24px] font-bold">98 180-66-55</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[12px] lg:text-[15px]">Электронная почта:</p>
+                    <span className="text-[12px] lg:text-[24px] font-bold">98 180-66-55</span>
+                  </div>
+                </div>
+                <div className="">
+                  <img src="/image.png" alt="" className="hidden lg:block lg:mt-[10px]"/>
+                </div>
+              </div>
+
+              <div className="block lg:hidden">
+                <h4 className="text-[18px] font-bold">Контактная информация</h4>
+                <div className="flex items-center gap-[15px] mt-[19px]">
+                  <div className="flex flex-col">
+                    <p className="text-[12px]">Отдел продаж:</p>
+                    <span className="text-[12px] font-bold">98 180-66-55</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-[12px]">Отдел контроля качества:</p>
+                    <span className="text-[12px] font-bold">98 180-66-55</span>
+                  </div>
+                </div>
+                <div className="">
+                  <img src="/image.png" alt="" className="hidden lg:block lg:mt-[10px]"/>
+                </div>
+              </div>
             </div>
 
-            <div className="flex flex-col bg-green-500 text-white">
-                <div className="bg-[url('/kefir.png')] h-32 sm:h-40 bg-cover"></div>
-
-                <div className="flex flex-col lg:flex-row w-full justify-between items-center px-6 sm:px-10 py-10 gap-10">
-                    
-                    <div>
-                        <img className="max-w-40" src="/logo.png" alt="Логотип" />
-                    </div>
-
-                    <div className="flex flex-col text-center lg:text-left">
-                        <h1 className="text-xl sm:text-2xl font-bold">Меню</h1>
-                        <div className="grid grid-cols-2 gap-4 mt-3 text-lg font-medium">
-                            <p>Продукция</p>
-                            <p>О компании</p>
-                            <p>Рецепты</p>
-                            <p>Вакансии</p>
-                            <p>Блог</p>
-                            <p>Контакты</p>
-                        </div>
-                    </div>
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-xl sm:text-2xl font-bold">Контактная информация</h1>
-                        <div className="mt-3 text-lg font-medium">
-                            <p>Поддержка: <span className="font-bold">98 180-66-55</span></p>
-                            <p className="mt-2">Электронная почта: <span className="font-bold">98 180-66-55</span></p>
-                        </div>
-                    </div>
-                </div>
+            <div className="flex items-center justify-around mt-[24px] lg:hidden">
+                <Link to={"/"}><img src="/logo.png" alt="" className="cursor-pointer"/></Link>
+                <img src="/image.png" alt="" />
             </div>
-        </>
-    );
-};
 
+            <div className="flex items-center justify-around mt-[15px] lg:mt-[53px] text-[10px] lg:text-[20px]">
+              <p>Puremilky 2025</p>
+              <a href="#" className="hover:underline">Политика Конфиденциальности</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    )
+}
+  
 export default Footer;
